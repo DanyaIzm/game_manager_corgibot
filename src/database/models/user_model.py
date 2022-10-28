29818fe70ea5d85cc68063d9aff8085bc0a92431
Daniel <danizmd8@gmail.com>
2022-10-28 14:58:21 +0300

    [Fix] Fixed user model
    Firstname and lastname should be optional

--- src/database/models/user_model.py
+++ src/database/models/user_model.py
@@ -7,8 +7,8 @@ def create_user_model(database: orm.Database) -> database.Entity:
     class UserModel(database.Entity):
         id = orm.PrimaryKey(int)
         username = orm.Optional(str, nullable=True)
-        first_name = orm.Required(str, nullable=True)
-        last_name = orm.Required(str, nullable=True)
+        first_name = orm.Optional(str, nullable=True)
+        last_name = orm.Optional(str, nullable=True)
         is_admin = orm.Required(bool, default=False)
 
     return UserModel
